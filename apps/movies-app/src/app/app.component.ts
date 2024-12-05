dccd1880eb0e94667ff5a69c40b8024372539865
Jurgen Van De Water <jvdw@abiware.be>
2024-12-05 10:10:30 +0100

    generator

--- apps/movies-app/src/app/app.component.ts
+++ apps/movies-app/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { AppShellComponent } from './app-shell/app-shell.component';
-
 @Component({
   standalone: true,
   imports: [RouterOutlet, AppShellComponent],
--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -6,19 +6,26 @@ import {
 } from '@nx/devkit';
 import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
+import { libraryGenerator } from '@nx/js';
 
 export async function utilLibGenerator(
   tree: Tree,
   options: UtilLibGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  // const projectRoot = `libs/${options.name}`;
+  // addProjectConfiguration(tree, options.name, {
+  //   root: projectRoot,
+  //   projectType: 'library',
+  //   sourceRoot: `${projectRoot}/src`,
+  //   targets: {},
+  // });
+  // generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
+  await libraryGenerator(tree, {
+    directory: 'libs/' + options.directory + '/util-' + options.name,
+    tags: 'type:util, scope:' + options.directory,
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
   await formatFiles(tree);
 }
 
