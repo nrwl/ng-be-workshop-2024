8bd0e387c099892d463f0de988401931e4993634
StevenVanbrielRosa <105644443+StevenVanbrielRosa@users.noreply.github.com>
2024-12-05 15:17:21 +0100

    feat(movies-app): added title

--- apps/movies-app/src/app/app.routes.ts
+++ apps/movies-app/src/app/app.routes.ts
@@ -5,47 +5,54 @@ export const routes: Routes = [
     path: 'list/:category',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-list').then(
-        m => m.MovieListPageComponent
+        (m) => m.MovieListPageComponent
       ),
+    title: 'Movies List',
   },
   {
     path: 'list/genre/:id',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-list').then(
-        m => m.MovieListPageComponent
+        (m) => m.MovieListPageComponent
       ),
+    title: 'Movies Genre',
   },
   {
     path: 'movie/:id',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-detail').then(
-        m => m.MovieDetailPageComponent
+        (m) => m.MovieDetailPageComponent
       ),
+    title: 'Movie Detail',
   },
   {
     path: 'search/:query',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-search').then(
-        m => m.MovieSearchPageComponent
+        (m) => m.MovieSearchPageComponent
       ),
+    title: 'Movie Search Query',
   },
   {
     path: 'my-movies',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-my-movie-list').then(
-        m => m.MyMovieListComponent
+        (m) => m.MyMovieListComponent
       ),
+    title: 'My Movies',
   },
   {
     path: '',
     redirectTo: 'list/popular',
     pathMatch: 'full',
+    title: 'Movies Popular List',
   },
   {
     path: '**',
     loadComponent: () =>
       import('@nx-workshop/shared/feature-not-found-page').then(
-        m => m.NotFoundPageComponent
+        (m) => m.NotFoundPageComponent
       ),
+    title: 'Not Found',
   },
 ];
