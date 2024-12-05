622549df25aadb3363577bcdbe1c7faf49950eba
StevenVanbrielRosa <105644443+StevenVanbrielRosa@users.noreply.github.com>
2024-12-05 11:11:23 +0100

    chore(update-scope-schema): update schema

--- libs/internal-plugin/src/generators/util-update-scope-schema/generator.ts
+++ libs/internal-plugin/src/generators/util-update-scope-schema/generator.ts
@@ -2,6 +2,8 @@ import {
   addProjectConfiguration,
   formatFiles,
   generateFiles,
+  getProjects,
+  ProjectConfiguration,
   Tree,
   updateJson,
 } from '@nx/devkit';
@@ -24,7 +26,40 @@ export async function updateScopeSchemaGenerator(
     ...json,
     defaultProject: 'movies-app',
   }));
+  const scopes = getScopes(getProjects(tree));
+  updateJson(
+    tree,
+    '/libs/internal-plugin/src/generators/util-lib/schema.json',
+    (schemaJson) => {
+      schemaJson.properties.directory['x-prompt'].items = scopes.map(
+        (scope) => ({
+          value: scope,
+          label: scope,
+        })
+      );
+      schemaJson.properties.directory.enums = scopes;
+      return schemaJson;
+    }
+  );
   await formatFiles(tree);
 }
 
 export default updateScopeSchemaGenerator;
+
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const allScopes: string[] = Array.from(projectMap.values())
+    .map((project) => {
+      if (project.tags) {
+        const scopes = project.tags.filter((tag: string) =>
+          tag.startsWith('scope:')
+        );
+        return scopes;
+      }
+      return [];
+    })
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+
+  // remove duplicates
+  return Array.from(new Set(allScopes));
+}
