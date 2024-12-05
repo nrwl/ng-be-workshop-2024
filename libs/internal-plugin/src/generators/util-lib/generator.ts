7136aa2d2e3d90f88d8fa9c57f1f888ba9b0b4a8
StevenVanbrielRosa <105644443+StevenVanbrielRosa@users.noreply.github.com>
2024-12-05 09:59:40 +0100

    chore(generator): add directory

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -6,19 +6,25 @@ import {
 } from '@nx/devkit';
 import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
+import { libraryGenerator } from '@nx/js';
 
 export async function utilLibGenerator(
   tree: Tree,
   options: UtilLibGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  console.log(options.name);
+  await libraryGenerator(tree, {
+    directory: 'libs/' + options.directory + '/util-' + options.name,
+    tags: 'type:util, scope:' + options.directory,
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  // const projectRoot = `libs/${options.name}`;
+  // addProjectConfiguration(tree, options.name, {
+  //   root: projectRoot,
+  //   projectType: 'library',
+  //   sourceRoot: `${projectRoot}/src`,
+  //   targets: {},
+  // });
+  // generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
 }
 
